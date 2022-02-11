1026b1d3d162e4a3094d85de5d114d7e758194a8
Toporin <satochip.wallet@gmail.com>
2022-02-11 18:39:56 +0100

    Satodime-Tool v0.2.0
    Add (basic) NFT support via Rarible
    -Display NFT info: name, description & image preview
    -Also rearrange show_details() menu
    
    Use segwit address by default when supported
    Legacy address will be deprecated in future version.
    Legacy addresses should NOT be used, and are shown only for backward compatibility.
    This allows to simplify interface, and avoid user confusion in the future
    
    UI: reorganize Seal Keyslot menu
    * Select coin first
    * Depending on coin, define asset as 'Coin' or allows selection among 'coin', 'Token', & 'NFT'
    * Entropy can be anything, it is hashed if more than 32 bytes long
    * TokenID must be a decimal number, not Hex!
    * Remove metadata (not used currently - RFU)
    
    Show ownership status in main menu

--- SatodimeTool/version.py
+++ SatodimeTool/version.py
@@ -1,3 +1,4 @@
 # version history:
 # v0.1.0: initial release
-SATODIMETOOL_VERSION = '0.1.0' # version of the client package
+# v0.2.0: use Segwit by default & refactor sealKeyslot and show details menus & add basic NFT support (via Rarible)
+SATODIMETOOL_VERSION = '0.2.0' # version of the client package
